9d614e64a226a58c8b65f70629a010d50d599991
Josh Parkinson <parkinsonxc@gmail.com>
2017-10-02 19:09:43 -0500

    Assignment passed

--- main.py
+++ main.py
@@ -26,17 +26,16 @@ def validate_input():
     if len(username) <= 3:
         username = ""
         username_error = "This username is too short."
-        #return redirect("/?username_error=" + username_error)
+
 
     elif len(username) > 20:
         username = ""
         username_error = "This username is too long."
-        #return redirect("/?username_error=" + username_error)
 
     elif ' ' in username:
         username = ""
         username_error = "Your username cannot have a space."
-        #return redirect("/?username_error=" + username_error)
+
 
         
     #PASSWORD CHECK
@@ -44,26 +43,24 @@ def validate_input():
         password = ""
         verified_password = ""
         password_error = "This password is too short."
-        #return redirect("/?password_error=" + password_error)
 
     elif len(password) > 20:
         password = ""
         verified_password = ""
         password_error = "This password is too long."
-        #return redirect("/?password_error=" + password_error)
 
     elif ' ' in password:
         password = ""
         verified_password = ""
         password_error = "Your password cannot have a space."
-        #return redirect("/?password_error=" + password_error)
+
 
     #VERIFY CHECK
     if password != verified_password:
         password = ""
         verified_password = ""
         verified_error = "Your passwords don't match."
-        #return redirect("/?verified_error=" + verified_error)
+
 
     #EMAIL IS OPTIONAL, SO WE HAVE TO SEE IF THEY FILLED IN THE BOX OR NOT BY CHECKING LENGTH
     if len(email) != 0:
@@ -72,12 +69,12 @@ def validate_input():
         if email.count('@') != 1 or email.count('@') > 1:
             email = ""
             email_error = "Invalid email."
-            #return redirect("/?email_error=" + email_error)
+
 
         elif email.count('.') != 1 or email.count('.') > 1:
             email = ""
             email_error = "Invalid email."
-            #return redirect("/?email_error=" + email_error)
+            
 
         elif len(email) <= 3 or len(email) > 20:
             email = ""
@@ -89,6 +86,7 @@ def validate_input():
     else:
          return render_template("signup.html",
         username=username,
+        email=email,
         username_error=username_error,
         password_error=password_error,
         verified_error=verified_error,
